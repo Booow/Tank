5046706ad2b3783383e303830f99234a2190d931
Booow <asasd60018@yahoo.com.tw>
2021-12-14 21:33:33 +0800

    Add tank shell RPC

--- Tank/Assets/Scripts/TankPlayer.cs
+++ Tank/Assets/Scripts/TankPlayer.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class TankPlayer : MonoBehaviourPunCallbacks
+namespace Tanks
 {
-    // Reference to tank's movement script, used to disable and enable control.
-    private Complete.TankMovement m_Movement;
-    // Reference to tank's shooting script, used to disable and enable control.
-    private Complete.TankShooting m_Shooting;
-    private void Awake()
+    public class TankPlayer : MonoBehaviourPunCallbacks
     {
-        m_Movement = GetComponent<Complete.TankMovement>();
-        m_Shooting = GetComponent<Complete.TankShooting>();
-        if (!photonView.IsMine)
+        // Reference to tank's movement script, used to disable and enable control.
+        private Complete.TankMovement m_Movement;
+        // Reference to tank's shooting script, used to disable and enable control.
+        private Complete.TankShooting m_Shooting;
+        private void Awake()
         {
-            m_Movement.enabled = false;
-            m_Shooting.enabled = false;
-            enabled = false;
+            m_Movement = GetComponent<Complete.TankMovement>();
+            m_Shooting = GetComponent<Complete.TankShooting>();
+            if (!photonView.IsMine)
+            {
+                m_Movement.enabled = false;
+                m_Shooting.enabled = false;
+                enabled = false;
+            }
         }
     }
 }
